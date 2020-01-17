bc900a1e5697cf68603c7b16fe517ee04694481a
Umar Nawawi <umar@ruangguru.com>
2020-01-17 12:28:54 +0700

    hotfix: rclone status

--- server/server_rclone.go
+++ server/server_rclone.go
@@ -1,15 +1,38 @@
 package server
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"os/exec"
+	"time"
 )
 
 func (s *Server) rclone() string {
 	cmd := exec.Command("sh", ".rclone.sh")
-	go cmd.CombinedOutput()
-	// if len(stdoutStderr) > 0 {
-	// 	log.Printf(string([]byte(stdoutStderr)))
-	// }
-	// return string([]byte(stdoutStderr))
-	return ""
+	go cmd.Start()
+	time.Sleep(100 * time.Millisecond)
+
+	rcloneStat := "/tmp/.rclone.stat"
+	res := ""
+
+	if fileExists(rcloneStat) {
+		stat := readPIDFile(rcloneStat)
+		res = string(stat)
+	} else {
+		res = string("Error reading " + rcloneStat)
+		log.Printf(res)
+	}
+	return res
+}
+
+func readPIDFile(name string) string {
+	file, err := os.Open(name)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	defer file.Close()
+
+	res, _ := ioutil.ReadAll(file)
+	return string(res)
 }
